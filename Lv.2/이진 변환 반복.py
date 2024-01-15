a32f1524685d05000c227ddce84bd573fbb05eb6
kangdohee <kangdohee1211@naver.com>
2024-01-15 21:56:31 +0900

    Create 이진 변환 반복.py

---
+++ Lv.2/이진 변환 반복.py
@@ -0,0 +1,15 @@
+def solution(s):
+  answer = []
+  cnt = 0
+  zeroCnt = 0
+
+  while True:
+    if s == "1";
+        break
+    zeroCnt += s.count("0")
+    s = "1"*(s.count("1"))
+    s = bin(len(s))[2:]
+
+    cnt += 1
+  answer = [cnt, zeroCnt]
+  return answer
